079d54f0b436ea2348c149c21d84344fb078ba99
Talha Ocakci <Talha.Ocakci@commencis.com>
2019-06-12 09:31:05 +0300

    Initial

---
+++ src/main/webapp/app/entities/employee/employee-detail.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link, RouteComponentProps } from 'react-router-dom';
+import { Button, Row, Col } from 'reactstrap';
+// tslint:disable-next-line:no-unused-variable
+import { Translate, ICrudGetAction } from 'react-jhipster';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { IRootState } from 'app/shared/reducers';
+import { getEntity } from './employee.reducer';
+import { IEmployee } from 'app/shared/model/employee.model';
+// tslint:disable-next-line:no-unused-variable
+import { APP_DATE_FORMAT, APP_LOCAL_DATE_FORMAT } from 'app/config/constants';
+
+export interface IEmployeeDetailProps extends StateProps, DispatchProps, RouteComponentProps<{ id: string }> {}
+
+export class EmployeeDetail extends React.Component<IEmployeeDetailProps> {
+  componentDidMount() {
+    this.props.getEntity(this.props.match.params.id);
+  }
+
+  render() {
+    const { employeeEntity } = this.props;
+    return (
+      <Row>
+        <Col md="8">
+          <h2>
+            <Translate contentKey="conferunsApp.employee.detail.title">Employee</Translate> [<b>{employeeEntity.id}</b>]
+          </h2>
+          <dl className="jh-entity-details" />
+          <Button tag={Link} to="/entity/employee" replace color="info">
+            <FontAwesomeIcon icon="arrow-left" />{' '}
+            <span className="d-none d-md-inline">
+              <Translate contentKey="entity.action.back">Back</Translate>
+            </span>
+          </Button>
+          &nbsp;
+          <Button tag={Link} to={`/entity/employee/${employeeEntity.id}/edit`} replace color="primary">
+            <FontAwesomeIcon icon="pencil-alt" />{' '}
+            <span className="d-none d-md-inline">
+              <Translate contentKey="entity.action.edit">Edit</Translate>
+            </span>
+          </Button>
+        </Col>
+      </Row>
+    );
+  }
+}
+
+const mapStateToProps = ({ employee }: IRootState) => ({
+  employeeEntity: employee.entity
+});
+
+const mapDispatchToProps = { getEntity };
+
+type StateProps = ReturnType<typeof mapStateToProps>;
+type DispatchProps = typeof mapDispatchToProps;
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(EmployeeDetail);
