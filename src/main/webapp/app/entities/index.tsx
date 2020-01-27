98cd5292c0257bb2fbd0df5109d5b8dffa80e3ac
Talha Ocakçı <tocakci@twilio.com>
2020-01-27 11:51:15 +0200

    Initial application generated by JHipster-5.8.2

--- src/main/webapp/app/entities/index.tsx
+++ src/main/webapp/app/entities/index.tsx
@@ -4,40 +4,12 @@ import { Switch } from 'react-router-dom';
 // tslint:disable-next-line:no-unused-variable
 import ErrorBoundaryRoute from 'app/shared/error/error-boundary-route';
 
-import Conference from './conferuns/conference';
-import Fee from './conferuns/fee';
-import File from './conferuns/file';
-import FileReview from './conferuns/file-review';
-import Place from './conferuns/place';
-import Presenter from './conferuns/presenter';
-import Room from './conferuns/room';
-import ScheduleItem from './conferuns/schedule-item';
-import Subject from './conferuns/subject';
-import Talk from './conferuns/talk';
-import TalkParticipant from './conferuns/talk-participant';
-import TalkTag from './conferuns/talk-tag';
-import Topic from './conferuns/topic';
-import TalkHistory from './conferuns/talk-history';
 /* jhipster-needle-add-route-import - JHipster will add routes here */
 
 const Routes = ({ match }) => (
   <div>
     <Switch>
       {/* prettier-ignore */}
-      <ErrorBoundaryRoute path={`${match.url}/conference`} component={Conference} />
-      <ErrorBoundaryRoute path={`${match.url}/fee`} component={Fee} />
-      <ErrorBoundaryRoute path={`${match.url}/file`} component={File} />
-      <ErrorBoundaryRoute path={`${match.url}/file-review`} component={FileReview} />
-      <ErrorBoundaryRoute path={`${match.url}/place`} component={Place} />
-      <ErrorBoundaryRoute path={`${match.url}/presenter`} component={Presenter} />
-      <ErrorBoundaryRoute path={`${match.url}/room`} component={Room} />
-      <ErrorBoundaryRoute path={`${match.url}/schedule-item`} component={ScheduleItem} />
-      <ErrorBoundaryRoute path={`${match.url}/subject`} component={Subject} />
-      <ErrorBoundaryRoute path={`${match.url}/talk`} component={Talk} />
-      <ErrorBoundaryRoute path={`${match.url}/talk-participant`} component={TalkParticipant} />
-      <ErrorBoundaryRoute path={`${match.url}/talk-tag`} component={TalkTag} />
-      <ErrorBoundaryRoute path={`${match.url}/topic`} component={Topic} />
-      <ErrorBoundaryRoute path={`${match.url}/talk-history`} component={TalkHistory} />
       {/* jhipster-needle-add-route-path - JHipster will routes here */}
     </Switch>
   </div>
--- src/main/webapp/app/shared/reducers/index.ts
+++ src/main/webapp/app/shared/reducers/index.ts
@@ -12,62 +12,6 @@ import activate, { ActivateState } from 'app/modules/account/activate/activate.r
 import password, { PasswordState } from 'app/modules/account/password/password.reducer';
 import settings, { SettingsState } from 'app/modules/account/settings/settings.reducer';
 import passwordReset, { PasswordResetState } from 'app/modules/account/password-reset/password-reset.reducer';
-// prettier-ignore
-import conference, {
-  ConferenceState
-} from 'app/entities/conferuns/conference/conference.reducer';
-// prettier-ignore
-import fee, {
-  FeeState
-} from 'app/entities/conferuns/fee/fee.reducer';
-// prettier-ignore
-import file, {
-  FileState
-} from 'app/entities/conferuns/file/file.reducer';
-// prettier-ignore
-import fileReview, {
-  FileReviewState
-} from 'app/entities/conferuns/file-review/file-review.reducer';
-// prettier-ignore
-import place, {
-  PlaceState
-} from 'app/entities/conferuns/place/place.reducer';
-// prettier-ignore
-import presenter, {
-  PresenterState
-} from 'app/entities/conferuns/presenter/presenter.reducer';
-// prettier-ignore
-import room, {
-  RoomState
-} from 'app/entities/conferuns/room/room.reducer';
-// prettier-ignore
-import scheduleItem, {
-  ScheduleItemState
-} from 'app/entities/conferuns/schedule-item/schedule-item.reducer';
-// prettier-ignore
-import subject, {
-  SubjectState
-} from 'app/entities/conferuns/subject/subject.reducer';
-// prettier-ignore
-import talk, {
-  TalkState
-} from 'app/entities/conferuns/talk/talk.reducer';
-// prettier-ignore
-import talkParticipant, {
-  TalkParticipantState
-} from 'app/entities/conferuns/talk-participant/talk-participant.reducer';
-// prettier-ignore
-import talkTag, {
-  TalkTagState
-} from 'app/entities/conferuns/talk-tag/talk-tag.reducer';
-// prettier-ignore
-import topic, {
-  TopicState
-} from 'app/entities/conferuns/topic/topic.reducer';
-// prettier-ignore
-import talkHistory, {
-  TalkHistoryState
-} from 'app/entities/conferuns/talk-history/talk-history.reducer';
 /* jhipster-needle-add-reducer-import - JHipster will add reducer here */
 
 export interface IRootState {
@@ -81,20 +25,6 @@ export interface IRootState {
   readonly passwordReset: PasswordResetState;
   readonly password: PasswordState;
   readonly settings: SettingsState;
-  readonly conference: ConferenceState;
-  readonly fee: FeeState;
-  readonly file: FileState;
-  readonly fileReview: FileReviewState;
-  readonly place: PlaceState;
-  readonly presenter: PresenterState;
-  readonly room: RoomState;
-  readonly scheduleItem: ScheduleItemState;
-  readonly subject: SubjectState;
-  readonly talk: TalkState;
-  readonly talkParticipant: TalkParticipantState;
-  readonly talkTag: TalkTagState;
-  readonly topic: TopicState;
-  readonly talkHistory: TalkHistoryState;
   /* jhipster-needle-add-reducer-type - JHipster will add reducer type here */
   readonly loadingBar: any;
 }
@@ -110,20 +40,6 @@ const rootReducer = combineReducers<IRootState>({
   passwordReset,
   password,
   settings,
-  conference,
-  fee,
-  file,
-  fileReview,
-  place,
-  presenter,
-  room,
-  scheduleItem,
-  subject,
-  talk,
-  talkParticipant,
-  talkTag,
-  topic,
-  talkHistory,
   /* jhipster-needle-add-reducer-combine - JHipster will add reducer here */
   loadingBar
 });
